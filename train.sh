64ffe603c0ed4687701a6c34d30eb955f0dd3bcc
Parker Grey Addison <pgaddiso@ucsd.edu>
2019-12-09 15:40:11 -0800

    Adding arguments to resume training

--- train.sh
+++ train.sh
@@ -13,11 +13,14 @@ python train.py --dataroot $DATADIR \
                 --name face2fake \
                 --checkpoints_dir $CHECKDIR \
                 --model cycle_gan \
+                --display_id 0
                 --gpu_ids $GPUIDS \
                 --batch_size $BATCHSIZE \
                 --load_size $LOADSIZE \
                 --crop_size $CROPSIZE \
-                --preprocess $PREPROCESS
+                --preprocess $PREPROCESS \
+                $CONTINUE \
+                --epoch_count $EPOCHCOUNT
 #                 --gpu_ids $1 \
 #                 --batch_size $2 \
 #                 --load_size $3 \
