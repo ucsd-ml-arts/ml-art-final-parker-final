3b876f1165f42b2d233a5cea60521e2fd25de75f
Parker Grey Addison <pgaddiso@ucsd.edu>
2019-12-08 17:01:57 -0800

    Using parameters to specify gpus and batch size.

--- train.sh
+++ train.sh
@@ -8,13 +8,13 @@ cd pytorch-CycleGAN-and-pix2pix/
 
 pip install -r requirements.txt
 
-# Start training the model (assumes 2 GPUs)
+# Start training the model
 python train.py --dataroot $DATADIR \
                 --name face2fake \
-                --gpu_ids 0,1 \
+                --gpu_ids $1 \
                 --checkpoints_dir $CHECKDIR \
                 --model cycle_gan \
-                --batch_size 8 \
+                --batch_size $2 \
                 --load_size 1024 \
                 --crop_size 1024 \
                 --preprocess none
